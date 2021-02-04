c8b5e6bc690b4cb87a87b77e1969cbf4937dd5e0
Akif Manzoor <akifmanzoor124@gmail.com>
2021-02-04 12:34:24 -0500

    created query for allAuthors and created resolver for Author

---
+++ part8/index.js
@@ -0,0 +1,3 @@
+// FullStackOpen Part 8 GraphQL
+// The schema describes what queries the client can send to the server, what kind of parameters the queries can have, and what kind of data the queries return.
+
